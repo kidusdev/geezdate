8ea0f9d74440c9356a70c5a7eaca168e5a6f7b1a
Kidus Abrham <adulisdevelopers@gmail.com>
2023-10-04 00:37:40 +0300

    converting from GC to EC and vice versa

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Start Your Script Here");
+import { toEC, toGC } from "./convert";
+
+class GeezDate {
+  public static toEC = (date: Date) => toEC(date);
+  public static toGC = (date: { year: number; month: number; date: number }) => toGC(date);
+}
+
+export default GeezDate;
